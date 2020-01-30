55e2d34bdf1ecc065c5ad2de5516c4376d7dc2b2
Alex Lawrence <mail@alex-lawrence.com>
2020-01-30 13:08:55 +0100

    Fix export of TypeScript definitions

--- tasks/build.js
+++ tasks/build.js
@@ -3,20 +3,19 @@ const fs = require('fs');
 
 const exec = command => childProcess.execSync(command, { stdio: 'inherit' });
 
-fs.rmdirSync('./dist/src', { recursive: true });
-exec('./node_modules/.bin/tsc');
-exec('./node_modules/.bin/tsc --declaration --emitDeclarationOnly --out dist/adequate.js');
-exec(`./node_modules/.bin/rollup --no-strict -f iife -i ./dist/src/index.js -o ./dist/adequate.js`);
+fs.rmdirSync('./dist', { recursive: true });
+exec('node_modules/.bin/tsc --declaration --declarationDir dist/types');
+exec(`node_modules/.bin/rollup --no-strict -f iife -i ./dist/src/index.js -o ./dist/adequate.js`);
 exec(
-  `./node_modules/.bin/rollup --no-strict -f esm -i ./dist/src/index.module.js -o ./dist/adequate.module.js`
+  `node_modules/.bin/rollup --no-strict -f esm -i dist/src/index.module.js -o ./dist/adequate.module.js`
 );
 exec(
-  './node_modules/.bin/terser -cm --mangle-props regex=/_$/ -o ./dist/adequate.min.js ./dist/adequate.js'
+  'node_modules/.bin/terser -cm --mangle-props regex=/_$/ -o dist/adequate.min.js dist/adequate.js'
 );
 
 fs.writeFileSync(
-  './dist/adequate.min.js',
-  fs.readFileSync('./dist/adequate.min.js', 'utf-8').replace(/const /g, 'let ')
+  'dist/adequate.min.js',
+  fs.readFileSync('dist/adequate.min.js', 'utf-8').replace(/const /g, 'let ')
 );
 
-fs.copyFileSync('./dist/adequate.min.js', './comparison/adequate@latest/adequate.min.js');
+fs.copyFileSync('dist/adequate.min.js', 'comparison/adequate@latest/adequate.min.js');
