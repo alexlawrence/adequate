e97451a7dba4a069d4c30d747d7c9a4583b8c980
alexlawrence <mail@alex-lawrence.com>
2020-02-02 22:46:12 +0100

    element: work in progress factoring

--- src/element.ts
+++ src/element.ts
@@ -36,8 +36,8 @@ const element = (render: RenderFunction, BaseClass = HTMLElement) => {
       const processedTokens = templateLiteralTokens.map(token =>
         token && (token as Function).call
           ? `return this.parentNode.closest('[scope]').f[${extractedFunctionExpressions.push(
-            token as Function
-          ) - 1}](...arguments)`
+              token as Function
+            ) - 1}](...arguments)`
           : token
       );
       self.f = extractedFunctionExpressions;
@@ -45,5 +45,109 @@ const element = (render: RenderFunction, BaseClass = HTMLElement) => {
     }
   };
 };
+
+/* type RenderFunction = (this: HTMLElement, element: HTMLElement) => TemplateTokenArray;
+type ExtractedFunctionExpressions = Function[];
+
+const element = (BaseElementClass = HTMLElement, render: RenderFunction = () => ['']) => {
+  return class extends BaseElementClass {
+    f!: ExtractedFunctionExpressions;
+    stateList_: any[];
+    renderArguments_: ReturnType<typeof createRenderArguments>;
+    customEventHandlers_: CustomEventHandlers;
+    render: any;
+
+    constructor() {
+      super();
+      this.stateList_ = [];
+      this.renderArguments_ = createRenderArguments(this);
+      this.customEventHandlers_ = {};
+      this.render = render;
+    }
+
+    connectedCallback() {
+      this.setAttribute('scope', '');
+      this.update();
+    }
+
+    update() {
+      const self = this;
+      const extractedFunctionExpressions: Function[] = [];
+      setStateScope(self.stateList_, () => window.requestAnimationFrame(() => self.update()));
+      updateCustomEventHandlers(self, this.customEventHandlers_);
+      const templateLiteralTokens = render.call(self, self);
+      const processedTokens = templateLiteralTokens.map(token =>
+        token && (token as Function).call
+          ? `return this.parentNode.closest('[scope]').f[${extractedFunctionExpressions.push(
+              token as Function
+            ) - 1}](...arguments)`
+          : token
+      );
+      self.f = extractedFunctionExpressions;
+      updateChildNodes(self, createFragment(processedTokens.join('')));
+    }
+  };
+}; */
+
+/**
+ * 
+better way detect adequate elements?
+what about the function name? adequate, element, ...=
+
+customElements.define(
+  'x-calculator',
+  adequate(HTMLElement, element => {
+    const [a, operator, b] = ['a', 'operator', 'b'].map(name => element.getAttribute(name));
+    const expression = `${a} ${operator} ${b}`;
+    return html`
+      <p>
+        ${expression} = ${new Function('return ' + expression)()}
+      </p>
+    `;
+  })
+);
+
+customElements.define(
+  'x-calculator',
+  class extends adequate(HTMLElement) {
+    render() {
+      const [a, operator, b] = ['a', 'operator', 'b'].map(name => this.getAttribute(name));
+      const expression = `${a} ${operator} ${b}`;
+      return html`
+        <p>
+          ${expression} = ${new Function('return ' + expression)()}
+        </p>
+      `;
+    }
+  }
+);
+
+customElements.define(
+  'x-counter',
+  adequate(HTMLElement, () => {
+    const [value, setValue] = useState(0);
+
+    return html`
+      <div>Counter: ${value}</div>
+      <button onclick="${() => setValue(value + 1)}">Increment</button>
+      <button onclick="${() => setValue(value - 1)}">Decrement</button>
+    `;
+  })
+);
+
+customElements.define(
+  'x-counter',
+  class extends Adequate(HTMLElement) {
+    render() {
+      const [value, setValue] = useState(0);
+      return html`
+        <div>Counter: ${value}</div>
+        <button onclick="${() => setValue(value + 1)}">Increment</button>
+        <button onclick="${() => setValue(value - 1)}">Decrement</button>
+      `;
+    }
+  }
+
+ */
 
 export { element };
