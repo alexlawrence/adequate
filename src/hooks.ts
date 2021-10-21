f7c54127d4a6d8bc48477a0595890b257c194052
Alex Lawrence <mail@alex-lawrence.com>
2021-10-21 21:10:46 +0200

    feat(hooks): reduce file size

--- src/hooks.ts
+++ src/hooks.ts
@@ -16,21 +16,20 @@ const useState = <T>(initialState: T): [T, (state: T) => void] => {
   const hookStack = currentHookStack;
   const index = currentIndex++;
   const sideEffect = currentSideEffect;
-  if (hookStack.length <= index) hookStack[index] = initialState;
+  if (!(index in hookStack)) hookStack[index] = initialState;
   const setState = (state: T) => sideEffect(hookStack[index] = state);
   return [hookStack[index], setState];
 };
 
 const useEffect = (effectFn: Function, dependencies: unknown[]) => {
-  const hookStack = currentHookStack;
   const index = currentIndex++;
-  const oldDependencies = hookStack[index];
+  const oldDependencies = currentHookStack[index];
   if (
     !oldDependencies ||
     dependencies.some((dependency, index) => dependency != oldDependencies[index])
   )
     queueMicrotask(effectFn as () => void);
-  hookStack[index] = dependencies;
+  currentHookStack[index] = dependencies;
 };
 
 export { useEffect, useState, withHooks };
